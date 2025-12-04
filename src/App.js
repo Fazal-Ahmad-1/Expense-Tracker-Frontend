15c3009bcc5982d68146009adca4277828c43759
Fazal Ahmad <fazalahmadofc@gmail.com>
2025-12-04 14:01:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "expensetrackerbackend-production-7e64.up.railway.app"; // update if needed
+const API_BASE_URL = "https://expensetrackerapplication-qt9a.onrender.com"; // update if needed
 
 function App() {
   const [username, setUsername] = useState("");
